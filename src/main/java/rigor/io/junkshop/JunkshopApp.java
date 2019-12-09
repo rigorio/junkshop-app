fc0b3299fd1f98b2bca69f4eb029aaa575747243
rigorei <rigorei.sarmiento@gmail.com>
2019-12-09 22:37:14 +0800

    Migrate panes to dashboards

--- src/main/java/rigor/io/junkshop/JunkshopApp.java
+++ src/main/java/rigor/io/junkshop/JunkshopApp.java
@@ -2,6 +2,7 @@ package rigor.io.junkshop;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import rigor.io.junkshop.dashboard.DashboardView;
 import rigor.io.junkshop.inventory.InventoryView;
 import rigor.io.junkshop.utils.GuiManager;
 
@@ -20,7 +21,7 @@ public class JunkshopApp extends Application {
     stage.setResizable(true);
 //    stage.getIcons().add(ngew Image("/img/.png"));
     MANAGER.setPrimaryStage(stage);
-    MANAGER.changeView(new InventoryView());
+    MANAGER.changeView(new DashboardView());
 //    MANAGER.changeView(new LoginView());
   }
 
