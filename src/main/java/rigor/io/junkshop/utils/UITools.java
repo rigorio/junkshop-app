65bcafb470382d2fe930667f57c032c472ed786f
rigorei <rigorei.sarmiento@gmail.com>
2020-01-14 12:27:06 +0800

    Quick loading alert

--- src/main/java/rigor/io/junkshop/utils/UITools.java
+++ src/main/java/rigor/io/junkshop/utils/UITools.java
@@ -1,6 +1,8 @@
 package rigor.io.junkshop.utils;
 
 import com.jfoenix.controls.JFXTextField;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
@@ -20,5 +22,15 @@ public class UITools {
     DecimalFormat format = new DecimalFormat("#.##");
     format.setRoundingMode(RoundingMode.CEILING);
     return format.format(dd);
+  }
+
+  public static Alert quickLoadingAlert() {
+    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+    alert.setTitle("Loading");
+    alert.setHeaderText(null);
+    alert.setGraphic(null);
+    alert.setContentText("Please wait..");
+    alert.getDialogPane().lookupButton(ButtonType.OK).setVisible(false);
+    return alert;
   }
 }
